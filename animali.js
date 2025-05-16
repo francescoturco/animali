1dcf696a59039753890063368fbcfe7948050717
fratur <francescoturco2795@gmail.com>
2025-05-16 18:22:21 +0200

    exercise-done

--- animali.js
+++ animali.js
@@ -12,6 +12,28 @@ Es:
    Crea un nuovo array con la lista dei mammiferi. 
 */
 
+//ARRAY CON OGGETTI
+const animali = [
+    {nome: 'leone', famiglia: 'felidi', classe: 'mammiferi'},
+    {nome: 'cane', famiglia: 'canidi', classe: 'mammiferi'},
+    {nome: 'gallina', famiglia: 'fasianidi', classe: 'uccelli'},
+    {nome: 'lucertola', famiglia: 'lacertidi', classe: 'rettili'},
+    {nome: 'rana', famiglia: 'ranidae', classe: 'anfibi'},
+
+
+];
+console.log(animali);
+
+
+//utilizzo la funzione .filter per cercare gli animali che appartegono alla classe mammiferi 
+const mammiferi = animali.filter(animale => animale.classe === 'mammiferi');
+
+console.log(mammiferi)
+    
+
+
+
+
 
 
 
